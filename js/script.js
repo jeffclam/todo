870dc7173b1860e5a1477ceb0f9e2b541d346504
Jeffrey Lam <jeffrey.clam95@gmail.com>
2018-02-26 09:36:42 -0800

    Click text to change its class from incomplete to complete

--- js/script.js
+++ js/script.js
@@ -1,20 +1,26 @@
+const $list = $("#todo_list");
+
+$list.on("click",".incomplete", function () {
+    console.log( $(this).text());
+    $(this).attr("class", "complete");
+});
+
 function submit(value) {
     if (value.length > 0) {
         console.log(value);
-        const list = $("#todo_list");
-        list.append('<li>'+value+'</li>')
+        $list.append('<li class="incomplete">'+value+'</li>')
     } else {
         alert("Submission must not be empty.");
     }
 }
 
-const input = $("#input");
-input.on("keydown", function(e) {
+const $input = $("#input");
+$input.on("keydown", function(e) {
     if (e.keyCode == 13) {
-        submit(input.val())
+        submit($input.val())
     }
 })
 
 $("#submit").on("click", function () {
-    submit(input.val());
+    submit($input.val());
 })
